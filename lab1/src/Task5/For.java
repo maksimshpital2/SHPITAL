a26b00f17b05283cf2b19dd07d8c4ef98ed19dce
maksimshpital2 <114862074+maksimshpital2@users.noreply.github.com>
2022-10-13 20:28:43 +0300

    Практическая 1

---
+++ lab1/src/Task5/For.java
@@ -0,0 +1,9 @@
+package Task5;
+
+public class For {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
---
+++ lab1/src/Task6/Harmonic.java
@@ -0,0 +1,10 @@
+package Task6;
+
+public class Harmonic {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(1. / i);
+        }
+
+    }
+}
