25f196be8516e3873d20ecc02b6c504b58f46489
maksimshpital2 <114862074+maksimshpital2@users.noreply.github.com>
2022-11-01 11:17:52 +0300

    Практическая 2

---
+++ lab2/src/Task2/Author.java
@@ -0,0 +1,34 @@
+package Task1;
+
+public class Author {
+    private final String name;
+    private String email;
+    private final char gender;
+
+    Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "Имя: " + name + ", Почта: " + email + ", Пол: " + gender + "}";
+    }
+}
