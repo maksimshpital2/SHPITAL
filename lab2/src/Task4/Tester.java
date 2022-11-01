c289da5851eaca8dd3103ce0e3312835b748fd0d
maksimshpital2 <114862074+maksimshpital2@users.noreply.github.com>
2022-11-01 11:23:44 +0300

    Практическая 2

---
+++ lab2/src/Task4/Tester.java
@@ -0,0 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+abstract class Tester {
+    public static void main(String[] args) {
+        Shop sh = new Shop(8);
+
+        System.out.println(sh);
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int a;
+            System.out.print("¬ведите номер компьютера (1-10): ");
+            a = scanner.nextInt();
+
+            sh.findComputer(a);
+            sh.addComputer(4);
+            sh.delComputer(5);
+            sh.findComputer(10);
+            System.out.println(sh.getAmount());
+            sh.setAmount(4);
+        } catch (InputMismatchException e) {
+            System.out.println("Error");
+            scanner.nextInt();
+        } finally {
+            scanner.close();
+        }
+    }
+}
