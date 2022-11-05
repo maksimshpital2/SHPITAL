ca14cf4e7c01c943e0e2318511ffd5c7733747b0
maksimshpital2 <114862074+maksimshpital2@users.noreply.github.com>
2022-11-05 13:53:16 +0300

    Удаляем

--- Custom Exception/Task1/INNisWrongException.java
+++
@@ -1,7 +0,0 @@
-package Task1;
-
-public class INNisWrongException extends Exception {
-    public INNisWrongException(String message) {
-        super(message);
-    }
-}
--- Custom Exception/Task2/FioswrongExecption.java
+++
@@ -1,7 +0,0 @@
-package Task2;
-
-public class FioiswrongException extends Exception {
-    public FioiswrongException(String message) {
-        super(message);
-    }
-}
