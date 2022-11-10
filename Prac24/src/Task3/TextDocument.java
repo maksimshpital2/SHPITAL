4dcfbe08a940b72002d7bb58d142c48b1a8a815c
maksimshpital2 <114862074+maksimshpital2@users.noreply.github.com>
2022-11-10 21:03:48 +0300

    Практическая 24

---
+++ Prac24/src/Task3/TextDocument.java
@@ -0,0 +1,14 @@
+package Task3;
+
+public class TextDocument implements IDocument {
+    private String string;
+
+    TextDocument(String string) {
+        this.string = string;
+    }
+
+    @Override
+    public String getString() {
+        return string;
+    }
+}
