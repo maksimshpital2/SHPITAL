3b830d731780bd6ae6720af264f25909b921cb4d
maksimshpital2 <114862074+maksimshpital2@users.noreply.github.com>
2022-11-13 17:14:41 +0300

    Delete Stek/src/Task1to2 directory

--- Stek/src/Task1to2/Main.java
+++
@@ -1,8 +0,0 @@
-package Task1to2;
-
-abstract class Main {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator("1 5 2 2 3 x 4 + / 1 - 7 / / /");
-        calculator.reading();
-    }
-}
