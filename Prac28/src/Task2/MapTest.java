f7653ac99fc30cfeb11300271800e6eb81c22f14
maksimshpital2 <114862074+maksimshpital2@users.noreply.github.com>
2022-12-06 18:41:03 +0300

    Практическая 28

---
+++ Prac28/src/Task2/MapTest.java
@@ -0,0 +1,9 @@
+package Task2;
+
+abstract class MapTest {
+    public static void main(String[] args) {
+        Maping<String, String> maping = new Maping<>();
+        System.out.println(maping.createMap());
+        System.out.println(maping.getSameFirstNameCount());
+    }
+}
