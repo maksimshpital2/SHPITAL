b086ae55b2357c408eb12d3cd151dcca4907668c
maksimshpital2 <114862074+maksimshpital2@users.noreply.github.com>
2022-12-06 19:27:58 +0300

    Delete Task2 directory

--- Task2/MapTest.java
+++
@@ -1,9 +0,0 @@
-package Task2;
-
-abstract class MapTest {
-    public static void main(String[] args) {
-        Maping<String, String> maping = new Maping<>();
-        System.out.println(maping.createMap());
-        System.out.println(maping.getSameFirstNameCount());
-    }
-}
